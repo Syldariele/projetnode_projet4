22cf2b1429559417c2b0b0e8db0e3cd32ed90b18
Sitael <beru666@live.fr>
2020-03-25 15:01:48 +0100

    configure mysql

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const mysql = require("mysql");
+const settings = require("./settings.json");
 
+const sqlConfig = settings.sqlConfig;
 const app = express();
+
 
 app.listen(3000, () => {
     console.log("SERVEUR STARTED !");
 });
 
 app.get("/", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+
+    sqlConnection.query(
+        " SELECT id, email, firstname, lastname, birthdate FROM node_users",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR :", error.code);
+            } else {
+                console.log("RESULT :", result);
+            }
+            sqlConnection.end();
+        }
+    );
+    
     res.send({
         id: 1,
         email: "beru666@live.fr",
